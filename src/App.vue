0d37e63d4baf1a461655925a52dc5a1e1dd8ada3
posiek07 <daniel.smolinski@hotmail.com>
2020-12-05 22:20:44 +0000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld :images="images" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 
+const data = [
+  require("./assets/photo-1458302488426-c9fe676e00b0.jpg"),
+  require("./assets/photo-1517842512476-c5a491eb1cf6.jpg"),
+  require("./assets/photo-1438183972690-6d4658e3290e.jpg"),
+  require("./assets/photo-1534297169727-a78dc2d3dea4.jpg"),
+  require("./assets/photo-1474366521946-c3d4b507abf2.jpg"),
+  require("./assets/photo-1505063111741-9b87489f9bd7.jpg"),
+  require("./assets/photo-1531137199527-9546e8290fd4.jpg"),
+  require("./assets/photo-1520699049698-acd2fccb8cc8.jpg"),
+  require("./assets/photo-1519682337058-a94d519337bc.jpg"),
+];
+
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+  data: () => {
+    return {
+      images: data,
+    };
+  },
 };
 </script>
 
@@ -23,6 +39,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: rgb(243, 243, 243);
+  align-content: center;
+  display: flex;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,117 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="d-flex justify-content-center">
+    <div class="justify-content-center">
+      <div class="p-3 d-flex buttons-container justify-content-center">
+        <div class="d-flex justify-content-end w-100 w-md-50">
+          <button class="btn btn-primary round mr-2">
+            <strong>Get the app</strong>
+          </button>
+          <button class="btn grey">
+            <strong>Log In</strong>
+          </button>
+        </div>
+      </div>
+      <div class="image-container d-flex justify-content-center">
+        <div class="w-100 w-md-50">
+          <div class="m-4">
+            <div class="d-block text-left">
+              <div class=" d-flex  align-items-center mb-3">
+                <h2 class="m-0">username</h2>
+                <button class="btn btn-outline-primary ml-4 pl-2 pr-2 follow">
+                  <strong>FOLLOW</strong>
+                </button>
+              </div>
+              <p class="mb-0">
+                <strong>Firstname Lastname</strong> Lorem ipsum dolor sit amet,
+                consectetur.
+              </p>
+              <a href="www.instagram.com" alt="Instagram">www.instagram.com</a>
+            </div>
+            <div
+              class="d-flex flex-row mt-3 w-100 w-md-50 justify-content-between"
+            >
+              <p class=""><strong>870</strong> posts</p>
+              <p class="ml-4"><strong>495</strong> followers</p>
+              <p class="ml-3"><strong>112</strong> posts</p>
+            </div>
+          </div>
+          <div>
+            <img
+              class="image m-2"
+              v-for="(image, i) in images"
+              :src="image"
+              :key="i"
+              @click="index = i"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <vue-gallery-slideshow
+      :images="images"
+      :index="index"
+      @close="index = null"
+    ></vue-gallery-slideshow>
   </div>
 </template>
 
 <script>
+import VueGallerySlideshow from "vue-gallery-slideshow";
+
 export default {
   name: "HelloWorld",
+
+  components: {
+    VueGallerySlideshow,
+  },
+  data: () => {
+    return {
+      index: null,
+    };
+  },
   props: {
-    msg: String
-  }
+    images: {
+      type: Array,
+      required: true,
+    },
+  },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.image-container {
+  background-color: rgb(243, 243, 243);
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.buttons-container {
+  background-color: rgb(255, 255, 255);
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.image {
+  width: 150px;
+  height: 150px;
+  background-size: cover;
+  cursor: pointer;
+  margin: 1em;
+  border-radius: 3px;
+  object-fit: cover;
 }
-a {
-  color: #42b983;
+.follow {
+  height: 100%;
+  padding: 3px;
+}
+.round {
+  border-radius: 20px;
+  padding: 0px 20px 0px 20px;
+}
+.grey {
+  color: gray;
+}
+@media (min-width: 768px) {
+  .w-md-50 {
+    width: 50% !important;
+  }
+  .image {
+    width: 210px;
+    height: 210px;
+  }
 }
 </style>
